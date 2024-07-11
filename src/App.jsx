453968839a76ef3c24a7272894c2ced2d748eca4
DidukhIvan <didukh.ivan@lll.kpi.ua>
2024-07-11 20:22:10 +0300

    remove reactlogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -10,9 +10,7 @@ function App() {
     <>
       <div>
         
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+       
       </div>
       <h1>Vite + React</h1>
       <div className="card">
